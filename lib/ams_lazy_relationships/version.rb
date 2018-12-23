9d6c099cbabb66797f53959e8fff6024b8df4ddf
Jan Bajena <bajena3@gmail.com>
2018-12-23 19:15:17 +0100

    Initial setup

---
+++ lib/ams_lazy_relationships/version.rb
@@ -0,0 +1,3 @@
+module AmsLazyRelationships
+  VERSION = "0.1.0"
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,24 @@
+require "simplecov"
+require "simplecov-lcov"
+SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
+SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
+SimpleCov.start do
+  add_filter(/^\/spec\//)
+end
+
+require "bundler/setup"
+require "ams_lazy_relationships"
+
+require "undercover"
+
+RSpec.configure do |config|
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = ".rspec_status"
+
+  # Disable RSpec exposing methods globally on `Module` and `main`
+  config.disable_monkey_patching!
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
