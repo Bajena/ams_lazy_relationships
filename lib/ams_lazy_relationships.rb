86857761e70cb020a81061d4dd31c5c49e3a4d03
Sergey Tokarenko <private.tokarenko.sergey@gmail.com>
2020-01-15 00:45:41 +0300

    Skip active_support as redundant dependency

--- lib/ams_lazy_relationships.rb
+++ lib/ams_lazy_relationships.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 require "batch-loader"
-
-# That is a missing dependency of AMS v0.10.0.rc4 in fact (
-require "active_support/core_ext/string/inflections"
 require "active_model_serializers"
 
 require "ams_lazy_relationships/version"
